d60d97b2d0a2246b676f2a831edab87d58022e7d
EricSpencer00 <espencer2@luc.edu>
2025-08-09 14:28:20 -0500

    other log screen library update

--- frontend/src/components/OtherLogForm.tsx
+++ frontend/src/components/OtherLogForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet, Alert, Text, Picker } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Alert, Text } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 import TimePicker from './common/TimePicker';
 
 
