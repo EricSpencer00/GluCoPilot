b5b9411ab8cc39860e4e5a55a94e7ffaaadaf2ae
EricSpencer00 <espencer2@luc.edu>
2025-08-11 00:13:44 -0500

    insights nav

--- frontend/src/navigation/InsightsNavigator.tsx
+++ frontend/src/navigation/InsightsNavigator.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { InsightsScreen } from '../screens/InsightsScreen';
-import { DetailedInsightScreen } from '../screens/DetailedInsightScreen';
+import DetailedInsightScreen from '../screens/DetailedInsightScreen';
 
 const Stack = createStackNavigator();
 
