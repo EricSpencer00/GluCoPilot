28c2089e5949c5060707e84c26cdc38174f79b7e
EricSpencer00 <espencer2@luc.edu>
2025-08-11 20:05:40 -0500

    basic loading screen

--- frontend/src/components/common/LoadingScreen.tsx
+++ frontend/src/components/common/LoadingScreen.tsx
@@ -1,29 +1,13 @@
 
 
-import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Dimensions, Image } from 'react-native';
-import { Text } from 'react-native-paper';
-
-const { width } = Dimensions.get('window');
-
-const loadingSmall = require('../../../assets/loading-small.gif');
-const loadingMedium = require('../../../assets/loading-medium.gif');
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { ActivityIndicator, Text } from 'react-native-paper';
 
 export const LoadingScreen: React.FC = () => {
-  const [showMedium, setShowMedium] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setShowMedium(true), 300);
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Image
-        source={showMedium ? loadingMedium : loadingSmall}
-        style={{ width: width * 0.7, height: width * 0.7 }}
-        resizeMode="contain"
-      />
+      <ActivityIndicator size="large" color="#fff" />
       <Text style={styles.text}>Loading...</Text>
     </View>
   );
