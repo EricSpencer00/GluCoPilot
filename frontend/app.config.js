64267499d00ff2f4d8182879e4a37f4d92db8dd3
EricSpencer00 <espencer2@luc.edu>
2025-08-14 14:27:32 -0500

    path

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -1,6 +1,9 @@
 // Generate development build config for Expo
 // This helps fix the notification limitations in Expo Go
 
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
   // Use EAS development build
   name: "GluCoPilot (Dev)",
@@ -31,6 +34,9 @@ module.exports = {
   extra: {
     eas: {
       projectId: "f16e8675-cf9b-4b3d-a4ba-58b21d990311"
-    }
+    },
+    GOOGLE_WEB_CLIENT_ID: process.env.GOOGLE_WEB_CLIENT_ID,
+    GOOGLE_IOS_CLIENT_ID: process.env.EXPO_GOOGLE_IOS_CLIENT_ID || process.env.GOOGLE_IOS_CLIENT,
+    GOOGLE_ANDROID_CLIENT_ID: process.env.EXPO_GOOGLE_ANDROID_CLIENT_ID || process.env.GOOGLE_ANDROID_CLIENT_ID,
   }
 };
