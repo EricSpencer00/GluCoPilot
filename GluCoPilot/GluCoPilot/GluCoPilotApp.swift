6da8ad269cad5c64c33457049a94af0e8e888348
ericspencer00 <espencer2@luc.edu>
2025-08-31 19:47:23 -0500

    xcode

---
+++ GluCoPilot/GluCoPilot/GluCoPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  GluCoPilotApp.swift
+//  GluCoPilot
+//
+//  Created by Eric Spencer on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct GluCoPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
